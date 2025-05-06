02df10cefc94e3c7a0be7eee3db36e0e0f5ffb67
CamilaJaviera91 <cjmunozn@hotmail.com>
2025-05-06 13:42:40 -0400

    feat(resume): create model, where we make a resume of all the tables

---
+++ mockdata/models/resume.sql
@@ -0,0 +1,30 @@
+{{ config(
+    materialized='table',
+    description='Sales summary by store and year, based on orders data.'
+) }}
+
+with resume as (
+    select 
+        o.order_date as date,
+        concat(s.city, '-', s.store_id) as store,
+        p.name,
+        p.category,
+        p.price,
+        o.quantity,
+        (p.price * o.quantity) as total,
+        sm.name as salesman,
+        c.name as client
+    from {{ ref('orders') }} o
+    join {{ ref('store') }} s on o.store_id = s.store_id
+    join {{ ref('product') }} p on o.product_id = p.product_id
+    join {{ ref('salesman') }} sm on o.salesman_id = sm.salesman_id
+    join {{ ref('client') }} c on o.client_id = c.client_id
+)
+
+select 
+    extract(year from date(date)) as year,
+    store,
+    sum(total) as total
+from resume
+group by year, store
+order by year, total asc
